36817cc1e2a7756b84137f04dcc6997f01cbac92
Nav Loveday <twelvetreeorchard@gmail.com>
2020-08-11 19:12:35 -0400

    working pokemon search

--- main.js
+++ main.js
@@ -1,37 +1,99 @@
-const kantoURL = 'https://pokeapi.co/api/v2/pokemon?limit=150';
-function fetchPokemon() {
-  fetch(kantoURL)
+// const kantoURL = 'https://pokeapi.co/api/v2/pokemon?limit=149';
+
+
+const pokemonForm = document.querySelector('#pokemonForm');
+pokemonForm.addEventListener('submit', fetchPokemon)
+
+let singleNum;
+
+function fetchPokemon(e) {
+  e.preventDefault();
+  let input = document.querySelector('#userInput');
+  let singleMan = input.value;
+  const baseURL = `https://pokeapi.co/api/v2/pokemon/${singleMan}/`;
+  fetch(baseURL)
     .then(result => result.json())
-    .then(kantoList => {
-      console.log(kantoList);
-      drawAllKanto(kantoList.results);
+    .then(data => {
+      console.log(data);
+      toPokedex(data);
     })
     .catch(err => console.log(err));
 }
+function toPokedex(pokemon){
+  const pokePic = document.querySelector('#pokePic');
+  const pokeStats = document.querySelector('#pokeStats');
+  const nameLi = document.createElement('li');
+  const typeLi = document.createElement('li');
+  // const flavorLi = document.createElement('li');
+  const newImg = document.createElement('img');
+  let types = '';
+  
+  pokePic.innerHTML = '';
+  pokeStats.innerHTML = '';
 
-function drawAllKanto(info) {
-  let ul = document.createElement('ul');
-  for (let i = 0; i < info.length; i++) {
-    let li = document.createElement('li');
-    let link = document.createElement('link');
-
-    console.log(pokemonURL);
-
-    li.innerText = info[i].name;
-    li.appendChild(link);
-    // console.log(link);
-
-    link.src = info[i].url;
-    ul.appendChild(li);
+  nameLi.innerText = pokemon.name;
+  nameLi.classList.add('capitalize');
+  for (let i = 0; i < pokemon.types.length; i++) {
+    types += `${pokemon.types[i].type.name} `;
   }
-  document.body.appendChild(ul)
+  typeLi.innerText = types;
+  typeLi.classList.add('capitalize');
+  // flavorLi.innerText = 
+  newImg.src = pokemon.sprites.front_default;
+
+  pokeStats.appendChild(nameLi);
+  pokeStats.appendChild(typeLi);
+  // pokeStats.appendChild(flavorLi);
+  pokePic.appendChild(newImg);
+
 }
-fetchPokemon();
-function fetchEach() {
-  let pokemonURL = info[i].url;
-  fetch(pokemonURL)
-    .then(result => result.json())
-    .then(pokemon => {
-      console.log(pokemon);
-    })
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function toPokedex(info) {
+//   let ul = document.createElement('ul');
+//   for (let i = 0; i < info.length; i++) {
+//     let li = document.createElement('li');
+//     let link = document.createElement('link');
+
+    
+
+//     li.innerText = info[i].name;
+//     li.appendChild(link);
+//     // console.log(link);
+
+//     link.src = info[i].url;
+//     ul.appendChild(li);
+//   }
+//   document.body.appendChild(ul)
+// }
