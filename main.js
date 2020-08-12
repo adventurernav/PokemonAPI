6ba952af6b6e8755291f8473de426ac236c385d9
Nav Loveday <twelvetreeorchard@gmail.com>
2020-08-12 17:38:31 -0400

    styled, initial submission

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-// const kantoURL = 'https://pokeapi.co/api/v2/pokemon?limit=149';
-
-
 const pokemonForm = document.querySelector('#pokemonForm');
 pokemonForm.addEventListener('submit', fetchPokemon)
-
 let singleNum;
-
 function fetchPokemon(e) {
   e.preventDefault();
   let input = document.querySelector('#userInput');
   let singleMan = input.value;
   const baseURL = `https://pokeapi.co/api/v2/pokemon/${singleMan}/`;
-  fetch(baseURL)
+   fetch(baseURL)
     .then(result => result.json())
     .then(data => {
       console.log(data);
       toPokedex(data);
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      alert('Pokemon not found. Please try again.');});
 }
 function toPokedex(pokemon){
   const pokePic = document.querySelector('#pokePic');
@@ -25,78 +22,20 @@ function toPokedex(pokemon){
   const numLi = document.createElement('li')
   const nameLi = document.createElement('li');
   const typeLi = document.createElement('li');
-  // const flavorLi = document.createElement('li');
   const newImg = document.createElement('img');
   let types = '';
-  
   pokePic.innerHTML = '';
   pokeStats.innerHTML = '';
-
-  numLi.innerText = pokemon.id;
+  numLi.innerText = '#' + pokemon.id;
   nameLi.innerText = pokemon.name;
   nameLi.classList.add('capitalize');
   for (let i = 0; i < pokemon.types.length; i++) {
-    types += `${pokemon.types[i].type.name} `;
-  }
-  typeLi.innerText = types;
+    types += `${pokemon.types[i].type.name} `;}
+  typeLi.innerText = `Types: ${types}`;
   typeLi.classList.add('capitalize');
-  // flavorLi.innerText = 
   newImg.src = pokemon.sprites.front_default;
-
   pokeStats.appendChild(numLi);
   pokeStats.appendChild(nameLi);
   pokeStats.appendChild(typeLi);
-  // pokeStats.appendChild(flavorLi);
   pokePic.appendChild(newImg);
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function toPokedex(info) {
-//   let ul = document.createElement('ul');
-//   for (let i = 0; i < info.length; i++) {
-//     let li = document.createElement('li');
-//     let link = document.createElement('link');
-
-    
-
-//     li.innerText = info[i].name;
-//     li.appendChild(link);
-//     // console.log(link);
-
-//     link.src = info[i].url;
-//     ul.appendChild(li);
-//   }
-//   document.body.appendChild(ul)
-// }
